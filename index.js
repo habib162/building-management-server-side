d6aec7776da729988d39e4f0dfae1aa0ad900360
habib162 <mrahman162123@bscse.uiu.ac.bd>
2023-11-27 01:17:18 +0600

    users created

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const userCollection = client.db("buildingdb").collection("users");
+
 
     app.post('/jwt',async(req,res) => {
         const user = req.body;
@@ -34,7 +36,18 @@ async function run() {
         res.send({ token });
     })
 
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await userCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'user already exists', insertedId: null })
+        }
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      });
 
+      
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
