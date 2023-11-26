2e4066c969b9cc3e6dd977bf305d1800e7089fce
habib162 <mrahman162123@bscse.uiu.ac.bd>
2023-11-26 22:58:03 +0600

    jwt token added

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    app.post('/jwt',async(req,res) => {
+        const user = req.body;
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN, {expiresIn: '1h'});
+        res.send({ token });
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
