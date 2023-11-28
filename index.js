6e34d2e1c87a597ccffd594dd9034d84f8e1d41a
habib162 <mrahman162123@bscse.uiu.ac.bd>
2023-11-29 01:35:40 +0600

    verify admin created

--- index.js
+++ index.js
@@ -54,8 +54,22 @@ async function run() {
         next();
       })
     }
+    
 
-    app.post('/users', async (req, res) => {
+    // admin verify 
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = {email : email};
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      next();
+    }
+
+
+    app.post('/users', verifyToken, verifyAdmin, async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
       const existingUser = await userCollection.findOne(query);
