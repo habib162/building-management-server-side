2f369cf74d193c708e716efd5d0aa73eee386860
habib162 <mrahman162123@bscse.uiu.ac.bd>
2023-11-29 19:37:57 +0600

    update role user

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6nq2qod.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -101,7 +101,20 @@ async function run() {
       }
       res.send({ admin });
     })
-
+    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const currentUser = await userCollection.findOne(filter);
+      const newRole = currentUser.role === 'user' ? 'member' : 'user';
+      console.log(currentUser.role);
+      const updatedDoc = {
+        $set: {
+          role: newRole
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
     
     app.get('/apartments', async (req, res) => {
       const result = await apartmentCollection.find().toArray();
